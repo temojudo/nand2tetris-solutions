571903b2247fb81e7c20db26b5973f19b362eac6
Temo Khutsishvili <tkhut18@freeuni.edu.ge>
2021-04-21 22:43:27 +0400

    completed project 4

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,80 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+// ----------------- pseudo code start -----------------
+// color = 0
+// LOOP1:
+//     M[addr] = SCREEN
+    
+//     if M[KBD] > 0:
+//         if color == -1:
+//             goto LOOP1
+//         color = -1
+//         goto LOOP2
+//     else:
+//         if color == 0:
+//             goto LOOP1
+//         color = 0
+
+//     LOOP2:
+//         if addr >= KBD goto LOOP1
+//         RAM[addr] = color // 1..1
+//         addr = addr + 1
+//         goto LOOP2
+// ----------------- pseudo code end -------------------
+
+(MAIN)
+    @color
+	M=0 // color = 0
+	
+    (LOOP1)
+        @SCREEN
+        D=A
+        @addr
+        M=D // addr = 16384
+        
+        @KBD
+        D=M
+        @COLOR_BLACK
+        D;JGT // if RAM[KBD] > 0 goto COLOR_BLACK
+
+        (COLOR_WHITE)
+            @color
+            D=M
+            @LOOP1
+            D;JEQ // if color == 0 goto LOOP1
+            
+            @color
+            M=0 // color = 0
+            
+            @LOOP2
+            0;JEQ // goto LOOP2
+
+        (COLOR_BLACK)
+            @color
+            D=M+1
+            @LOOP1
+            D;JEQ // if color == -1 goto LOOP1
+            
+            @color
+            M=-1 // color = -1
+        
+        (LOOP2)
+            @addr
+            D=M
+            @KBD
+            D=D-A
+            @LOOP1
+            D;JGE // if addr >= KBD goto LOOP1
+
+            @color
+            D=M
+            @addr
+            A=M
+            M=D // RAM[addr] = color
+            
+            @addr
+            M=M+1 // addr = addr + 1
+
+            @LOOP2
+            0;JMP // goto LOOP2
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,67 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+// ----------------- pseudo code start -----------------
+// a = R0
+// b = R1
+// i = 1
+// ans = 0
+
+// LOOP:
+//     if i > a goto STOP
+//     ans = ans + b
+//     i = i + 1
+//     goto LOOP
+// STOP:
+//     R2 = ans
+// ----------------- pseudo code end -------------------
+
+(MAIN)
+	@R0
+	D=M
+	@a
+	M=D // a = R0
+
+	@R1
+	D=M
+	@b
+	M=D // b = R1
+	
+	@R2
+	M=0 // M[2] = 0
+
+	@i
+	M=1 // i = 1
+	@ans
+	M=0 // ans = 0
+	
+	(LOOP)
+		@i
+		D=M
+		@a
+		D=D-M
+		@STOP
+		D;JGT // if i > a goto STOP
+		
+		@ans
+		D=M
+		@b
+		D=D+M
+		@ans
+		M=D // ans = ans + b
+		
+		@i
+		M=M+1 // i = i + 1
+		
+		@LOOP
+		0;JMP
+	
+	(STOP)
+		@ans
+		D=M
+		@R2
+		M=D // RAM[2] = ans
+
+	(END)
+		@END
+		0;JMP
